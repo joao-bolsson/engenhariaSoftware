562c6c852c2e7cb106664ed9cd98c4f549a48116
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-12-05 23:31:59 -0200

    first commit

---
+++ trabalho4/src/Aluno.java
@@ -0,0 +1,108 @@
+public class Aluno {
+
+    /**
+     * Nome do Aluno.
+     */
+    private final String nome;
+
+    /**
+     * Nota da primeira prova.
+     */
+    private final double notaA;
+
+    /**
+     * Nota da segunda prova.
+     */
+    private final double notaB;
+
+    /**
+     * Peso da primeira prova.
+     */
+    public static final int PESO_PROVA_1 = 4;
+
+    /**
+     * Peso da segunda prova.
+     */
+    public static final int PESO_PROVA_2 = 6;
+
+    /**
+     * Média mínima das duas provas que o Aluno precisa para ser aprovado.
+     */
+    private static final int MEDIA_MINIMA = 7;
+
+    /**
+     * Nota mínima que o Aluno precisa para ser aprovado com exame.
+     */
+    private static final int MEDIA_EXAME = 5;
+
+    /**
+     * Média do Aluno.
+     */
+    private double media;
+
+    /**
+     * Nota que o aluno tirou no exame (opcional).
+     */
+    private double notaExame;
+
+    /**
+     * Cria um Aluno.
+     *
+     * @param nome  Nome do aluno.
+     * @param notaA Primeira nota da prova.
+     * @param notaB Segunda nota da prova.
+     */
+    public Aluno(final String nome, final double notaA, final double notaB) {
+        this.nome = nome;
+        this.notaA = notaA;
+        this.notaB = notaB;
+        this.notaExame = 0;
+        setMedia();
+    }
+
+    /**
+     * Cria um Aluno que precisa de exame para tentar ser aprovado.
+     *
+     * @param nome      Nome do Aluno.
+     * @param notaA     Primeira nota da prova.
+     * @param notaB     Segunda nota da prova.
+     * @param notaExame Nota no exame.
+     */
+    public Aluno(final String nome, final double notaA, final double notaB, final double notaExame) {
+        this.nome = nome;
+        this.notaA = notaA;
+        this.notaB = notaB;
+        this.notaExame = notaExame;
+        setMedia();
+    }
+
+    private void setMedia() {
+        media = ((notaA * PESO_PROVA_1) + (notaB * PESO_PROVA_2)) / (PESO_PROVA_1 + PESO_PROVA_2);
+    }
+
+    /**
+     * @return Se o Aluno tem média superior ou igual à necessária - true, senão - false.
+     */
+    public boolean estaAprovado() {
+        System.out.println(nome + ", média: " + media);
+        return media >= MEDIA_MINIMA;
+    }
+
+    /**
+     * Cálculo para decidir se o aluno passou no exame: (media + exame) / 2 > MEDIA_EXAME
+     *
+     * @return Se o Aluno tem nota suficiente (com o exame) para ser aprovado - true, senão - false.
+     */
+    public boolean estaAprovadoComExame() {
+        if (!estaAprovado()) {
+            double mediaExame = (media + notaExame) / 2;
+            System.out.println(nome + ", média com exame: " + mediaExame);
+            return mediaExame > MEDIA_EXAME;
+        }
+        return true;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+}
---
+++ trabalho4/src/AlunoTest.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AlunoTest {
+
+    /**
+     * Testa o método está aprovado da classe Aluno.
+     */
+    @Test
+    void estaAprovado() {
+        System.out.println("[teste estaAprovado()]");
+        Aluno joao = new Aluno("João", 6, 4);
+        assertFalse(joao.estaAprovado());
+        double mediaEsperada = (double) ((6 * Aluno.PESO_PROVA_1) + (4 * Aluno.PESO_PROVA_2)) / (Aluno.PESO_PROVA_1 + Aluno.PESO_PROVA_2);
+        assertEquals(mediaEsperada, joao.getMedia());
+
+        Aluno maria = new Aluno("Maria", 7, 9);
+        assertTrue(maria.estaAprovado());
+
+        Aluno pedro = new Aluno("Pedro", 5, 9);
+        assertTrue(pedro.estaAprovado());
+    }
+
+    /**
+     * Testa o método está aprovado com exame da classe Aluno.
+     */
+    @Test
+    void estaAprovadoComExame() {
+        System.out.println("[teste estaAprovadoComExame()]");
+        Aluno joao = new Aluno("João", 6, 4, 6);
+        assertTrue(joao.estaAprovadoComExame()); // aprovado com exame
+
+        Aluno maria = new Aluno("Maria", 0, 0, 9);
+        assertFalse(maria.estaAprovadoComExame()); // precisava de 10
+
+        Aluno pedro = new Aluno("Pedro", 0, 1, 9.6);
+        assertTrue(pedro.estaAprovadoComExame());
+    }
+}
